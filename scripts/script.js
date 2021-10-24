4f4726bb1d8906c260782dd5c6f6228944f6db1c
Gabriel Costa <gabrielcosta12377@hotmail.com>
2021-10-24 06:04:39 -0300

    update

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,25 @@ let menu = document.getElementById("menu-bars")
 let navbar = document.getElementsByClassName("navbar")[0]
 let menuOpen = false
 
+let section = document.querySelectorAll("section")
+let navLinks = document.querySelectorAll("header .navbar a")
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY
+        let height = sec.offsetHeight
+        let offset = sec.offsetTop - 150
+        let id = sec.getAttribute('id')
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active')
+                document.querySelector("header .navbar a[href*=" + id + "]").classList.add('active')
+            })
+        }
+    })
+}
+
 function menuInitial() {
     menu.src = "./assets/bars.svg"
     navbar.style.display = "none"
@@ -49,3 +68,13 @@ let swiper = new Swiper(".home-slider", {
     },
     loop: true,
 })
+
+function loader() {
+    document.getElementsByClassName("loader-container")[0].classList.add('fade-out')
+}
+
+function fadeOut() {
+    setInterval(loader, 3000)
+}
+
+window.onload = fadeOut
